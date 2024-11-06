310ae85bc377a74b4f9a826565ea2d67fd85bba8
Davide Morazzo <davide.morazzo@protonmail.com>
2024-11-06 19:08:23 +0000

    PIC-8259 move

--- src/kernel/include/kernel/PIC-8259.h
+++ src/kernel/include/kernel/PIC-8259.h
@@ -37,72 +37,14 @@ arguments:
 		vectors on the master become offset1..offset1+7
 	offset2 - same for slave PIC: offset2..offset2+7
 */
-void PIC_remap(int offset1, int offset2)
-{
-	uint8_t a1, a2;
-	
-	a1 = inb(PIC1_DATA);                        // save masks
-	a2 = inb(PIC2_DATA);
-	
-	outb(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);  // starts the initialization sequence (in cascade mode)
-	wait_io();
-	outb(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
-	wait_io();
-	outb(PIC1_DATA, offset1);                 // ICW2: Master PIC vector offset
-	wait_io();
-	outb(PIC2_DATA, offset2);                 // ICW2: Slave PIC vector offset
-	wait_io();
-	outb(PIC1_DATA, 4);                       // ICW3: tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
-	wait_io();
-	outb(PIC2_DATA, 2);                       // ICW3: tell Slave PIC its cascade identity (0000 0010)
-	wait_io();
-	
-	outb(PIC1_DATA, ICW4_8086);               // ICW4: have the PICs use 8086 mode (and not 8080 mode)
-	wait_io();
-	outb(PIC2_DATA, ICW4_8086);
-	wait_io();
-	
-	outb(PIC1_DATA, a1);   // restore saved masks.
-	outb(PIC2_DATA, a2);
-}
+void PIC_remap(int offset1, int offset2);
 
 #define PIC_EOI		0x20		/* End-of-interrupt command code */
 
-void PIC_sendEOI(uint8_t irq)
-{
-	if(irq >= 8)
-		outb(PIC2_COMMAND,PIC_EOI);
-	
-	outb(PIC1_COMMAND,PIC_EOI);
-}
+void PIC_sendEOI(uint8_t irq);
 
+void IRQ_set_mask(uint8_t IRQline);
 
-void IRQ_set_mask(uint8_t IRQline) {
-    uint16_t port;
-    uint8_t value;
-
-    if(IRQline < 8) {
-        port = PIC1_DATA;
-    } else {
-        port = PIC2_DATA;
-        IRQline -= 8;
-    }
-    value = inb(port) | (1 << IRQline);
-    outb(port, value);        
-}
-
-void IRQ_clear_mask(uint8_t IRQline) {
-    uint16_t port;
-    uint8_t value;
-
-    if(IRQline < 8) {
-        port = PIC1_DATA;
-    } else {
-        port = PIC2_DATA;
-        IRQline -= 8;
-    }
-    value = inb(port) & ~(1 << IRQline);
-    outb(port, value);        
-}
+void IRQ_clear_mask(uint8_t IRQline);
 
 #endif
