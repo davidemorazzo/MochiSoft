ea3b43b2a888dcdaacd968ced6149fe624310624
Davide Morazzo <davide.morazzo@protonmail.com>
2024-11-06 10:52:25 +0000

    PIT initial commit

---
+++ src/kernel/include/dev/8253/PIT.h
@@ -0,0 +1,42 @@
+/* Novembre 2024 - Davide Morazzo
+
+Libreria per gestire il dispositivo PIT (Programmable Interrupt 
+Controller) modello 8253 o 8254.
+Fonti: 
+    - https://wiki.osdev.org/Programmable_Interval_Timer
+    - https://www.intel.com/content/dam/www/public/us/en/documents/manuals/64-ia-32-architectures-software-developer-vol-3a-part-1-manual.pdf
+
+I/O port     Usage
+0x40         Channel 0 data port (read/write) => Unico canale utilizzabile
+0x41         Channel 1 data port (read/write)
+0x42         Channel 2 data port (read/write)
+0x43         Mode/Command register (write only, a read is ignored)
+
+*/
+#ifndef INCLUDE_DEV_8253_PIT
+#define INCLUDE_DEV_8253_PIT 1
+
+
+
+#define PIT_CH0_DATA 0x40
+#define PIT_CH1_DATA 0x41
+#define PIT_CH2_DATA 0x42
+#define PIT_CMD_REG  0x43
+
+#define PIT_MODE_INT_ON_TERMINAL_COUNT 0
+#define PIT_MODE_HW_RETRIGG_ONESHOT 1
+#define PIT_MODE_RATE_GEN 2
+#define PIT_MODE_SQUARE_WAVE_GEN 3
+#define PIT_MODE_SW_TRIGG_STROBE 4
+#define PIT_MODE_HW_TRIGG_STROBE 5
+
+#define PIT_BASE_FREQ 1193182  // Hz
+
+void pit_set_count(uint8_t ch_addr, uint16_t count);
+uint16_t pit_read_count(uint8_t ch_addr);
+void pit_init();
+void pit_send_command(uint8_t command);
+
+#endif
+
+
---
+++ src/kernel/kernel/dev/8253/PIT.c
@@ -0,0 +1,51 @@
+#include "dev/8253/PIT.h"
+#include "kernel/PIC-8259.h"
+#include "kernel/microcode.h";
+
+void pit_set_count(uint8_t ch_addr, uint16_t count){
+    outb(ch_addr, count&0xFF);          // Low-byte
+    outb(ch_addr, count&0xFF >> 8);     // High-byte
+    return;
+}
+
+
+uint16_t pit_read_count(uint8_t ch_addr){
+    uint8_t read_back = 0x00;
+    uint8_t ch_select = 0x00;
+    uint16_t count = 0;
+    switch (ch_addr){
+        case PIT_CH0_DATA: ch_select = 0x2; break;
+        case PIT_CH1_DATA: ch_select = 0x4; break;
+        case PIT_CH2_DATA: ch_select = 0x8; break;
+    }
+    read_back |= 0xC0 | ch_select | 0x10; 
+    disable_it();
+        outb(PIT_CMD_REG, read_back);
+        count = inb(ch_addr);
+        count |= inb(ch_addr) << 8;
+    enable_it();
+    return count;
+}
+
+
+void pit_init(){
+    /* PIT init */
+    uint8_t init_cmd = 0x0;
+    // CH0, lobyte/hibyte access, mode rate gen
+    init_cmd |= 0x30 | (PIT_MODE_RATE_GEN << 1);
+    pit_send_command(init_cmd);
+    uint16_t reload_value = PIT_BASE_FREQ / 20; // 20Hz for IRQ0
+    outb(PIT_CH0_DATA, reload_value&0xFF);
+    outb(PIT_CH0_DATA, (reload_value&0xFF00 >> 8));
+
+    /* PIC enable IRQ0 */
+    IRQ_clear_mask(0);
+
+    return;
+}
+
+
+void pit_send_command(uint8_t command){
+    outb(PIT_CMD_REG, command);
+}
+
