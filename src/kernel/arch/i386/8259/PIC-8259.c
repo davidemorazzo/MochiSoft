ef81375439823503376c341c430e1596b11bb65d
Davide Morazzo <morazzo.davide@gmail.com>
2024-11-10 12:43:41 +0100

    PIC: read_IRR, read_ISR

--- src/kernel/arch/i386/8259/PIC-8259.c
+++ src/kernel/arch/i386/8259/PIC-8259.c
@@ -67,3 +67,28 @@ void IRQ_clear_mask(uint8_t IRQline) {
     value = inb(port) & ~(1 << IRQline);
     outb(port, value);        
 }
+
+#define PIC_READ_IRR                0x0a    /* OCW3 irq ready next CMD read */
+#define PIC_READ_ISR                0x0b    /* OCW3 irq service next CMD read */
+
+/* Helper func */
+static uint16_t __pic_get_irq_reg(int ocw3)
+{
+    /* OCW3 to PIC CMD to get the register values.  PIC2 is chained, and
+     * represents IRQs 8-15.  PIC1 is IRQs 0-7, with 2 being the chain */
+    outb(PIC1_COMMAND, ocw3);
+    outb(PIC2_COMMAND, ocw3);
+    return (inb(PIC2_COMMAND) << 8) | inb(PIC1_COMMAND);
+}
+
+/* Returns the combined value of the cascaded PICs irq request register */
+uint16_t pic_get_irr(void)
+{
+    return __pic_get_irq_reg(PIC_READ_IRR);
+}
+
+/* Returns the combined value of the cascaded PICs in-service register */
+uint16_t pic_get_isr(void)
+{
+    return __pic_get_irq_reg(PIC_READ_ISR);
+}
--- src/kernel/include/kernel/PIC-8259.h
+++ src/kernel/include/kernel/PIC-8259.h
@@ -47,4 +47,8 @@ void IRQ_set_mask(uint8_t IRQline);
 
 void IRQ_clear_mask(uint8_t IRQline);
 
+uint16_t pic_get_irr(void);
+
+uint16_t pic_get_isr(void);
+
 #endif
