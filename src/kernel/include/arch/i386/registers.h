53cc0fe099a66460508caca3ee198ce969fddd44
Davide Morazzo <morazzo.davide@gmail.com>
2025-05-07 16:32:31 +0200

    i386 registers struct

---
+++ src/kernel/include/arch/i386/registers.h
@@ -0,0 +1,26 @@
+/* Maggio 2025 - Davide Morazzo 		*/
+/* ------------------------------------	*/
+/* Definizioni per i registri specifici */
+/* Per CPU Intel 80386 (i386)			*/
+
+#ifndef INCLUDE_ARCH_I386_REGISTERS_H
+#define INCLUDE_ARCH_I386_REGISTERS_H 
+
+#include <stdint.h>
+
+typedef struct registers_s{
+	uint32_t eax;
+	uint32_t ecx;
+	uint32_t edx;
+	uint32_t ebx;
+	uint32_t esp;
+	uint32_t ebp;
+	uint32_t esi;
+	uint32_t edi;
+}registers_t;
+
+typedef struct eflags_s{
+	uint32_t eflags;
+}eflags_t;
+
+#endif
